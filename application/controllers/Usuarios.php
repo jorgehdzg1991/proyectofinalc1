0541b2012833fe6e3cce10c2f3e6ddc0d747f1e2
Jorge Hernández García <jorgehdzg1991@gmail.com>
2016-11-17 15:34:10 -0600

    Desarrollo parcial del módulo de administración de usuarios del sistema

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -10,11 +10,30 @@ class Usuarios extends MY_Controller
         $this->load->model("usuarios_model");
     }
 
-    public function index() {
+    public function index()
+    {
+        $usuarios = $this->usuarios_model->obtenerTodos();
 
+        $datosUsuarios = [];
+
+        foreach ($usuarios as $usuario) {
+            $fechaCreacion = new DateTime($usuario["fechaCreacion"]);
+
+            $usuarioCreador = $this->usuarios_model->obtenerPorId($usuario["usuarios_id"]);
+
+            $datosUsuarios[] = [
+                "id" => $usuario["id"],
+                "nombre" => $usuario["nombre"],
+                "perfil" => $usuario["perfil"] == 1 ? 'Administrador' : 'Usuario del sistema',
+                "login" => $usuario["login"],
+                "usuarioCreador" => $usuarioCreador["nombre"],
+                "fechaCreacion" => $fechaCreacion->format('d/m/Y H:i:s')
+            ];
+        }
 
         $datos = [
-            "titulo" => "Usuarios del sistema"
+            "titulo" => "Usuarios del sistema",
+            "usuarios" => $datosUsuarios
         ];
 
         $this->CargarVista("usuarios/index", $datos);
--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -14,5 +14,93 @@
 </div>
 
 <div class="container">
-
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4>Usuarios del sistema</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Nombre</th>
+                                <th>Inicio de sesión</th>
+                                <th>Perfil</th>
+                                <th>Creado por</th>
+                                <th>Fecha de creación</th>
+                                <th>Acciones</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            if (count($usuarios) > 0) {
+                                foreach ($usuarios as $usuario) {
+                                    echo '
+                                        <tr>
+                                            <td>' . $usuario["id"] . '</td>
+                                            <td>' . $usuario["nombre"] . '</td>
+                                            <td>' . $usuario["login"] . '</td>
+                                            <td>' . $usuario["perfil"] . '</td>
+                                            <td>' . $usuario["usuarioCreador"] . '</td>
+                                            <td>' . $usuario["fechaCreacion"] . '</td>
+                                            <td>
+                                                <a href="' . site_url("usuarios/editar/{$usuario["id"]}") . '"
+                                                   class="btn btn-default btn-sm"
+                                                   title="Editar usuario">
+                                                    <i class="fa fa-pencil"></i>
+                                                </a>
+                                                <a href="javascript: eliminarUsuario(\'' . site_url("usuarios/eliminar/{$usuario["id"]}") . '\', \'' . $usuario["nombre"] . '\');"
+                                                   class="btn btn-default btn-sm"
+                                                   title="Eliminar usuario">
+                                                    <i class="fa fa-times"></i>
+                                                </a>
+                                            </td>
+                                        </tr>';
+                                }
+                            } else {
+                                echo '
+                                    <tr>
+                                        <td colspan="5">
+                                            Actualmente no hay usuarios registrados en el sistema. Haz click en el
+                                            botón de abajo para crear uno nuevo.<br><br>
+                                            <a href="' . site_url("usuarios/crear") . '" class="btn btn-primary">Crear un usuario ahora</a>
+                                        </td>
+                                    </tr>';
+                            }
+                            ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script>
+    function eliminarUsuario(url, nombreUsuario) {
+        bootbox.confirm({
+            title: 'Eliminar usuario: ' + nombreUsuario,
+            message: '¿Realmente deseas eliminar este usuario?',
+            buttons: {
+                'cancel': {
+                    label: 'No realmente',
+                    className: 'btn-default'
+                },
+                'confirm': {
+                    label: 'Si, elimínalo',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function(result) {
+                if (result) {
+                    window.location = url;
+                }
+            }
+        });
+
+    }
+</script>
