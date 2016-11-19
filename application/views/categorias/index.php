b6437c4a3b8e8b86c3e4604135bf552e7c4855e6
Jorge Hernández García <jorgehdzg1991@gmail.com>
2016-11-19 09:18:03 -0600

    Se terminó el módulo de categorías

--- application/views/categorias/index.php
+++ application/views/categorias/index.php
@@ -3,7 +3,7 @@
         <li><a href="<?php echo site_url("inicio/index") ?>">Inicio</a></li>
         <li>Categorias</li>
     </ol>
-    <h1>Categorias</h1>
+    <h1>Categorias de producto</h1>
     <div class="options">
         <div class="btn-toolbar">
             <a href="<?php echo site_url("categorias/crear") ?>" class="btn btn-primary">
@@ -34,20 +34,33 @@
                             </thead>
                             <tbody>
                             <?php
-                            foreach ($categorias as $categorias) {
+                            if (count($categorias) > 0) {
+                                foreach ($categorias as $categorias) {
+                                    echo '
+                                    <tr>
+                                        <td>' . $categorias["id"] . '</td>
+                                        <td>' . $categorias["nombre"] . '</td>
+                                        <td>' . $categorias["usuario"] . '</td>
+                                        <td>' . $categorias["fecha"] . '</td>
+                                        <td>
+                                            <a href="' . site_url("categorias/editar/{$categorias["id"]}") . '" class="btn btn-default btn-sm" title="Editar categoría">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                            <a href="javascript:eliminarCategoria(\'' . site_url("categorias/eliminar/{$categorias["id"]}") . '\', \'' . $categorias["nombre"] . '\');"
+                                               class="btn btn-danger btn-sm"
+                                               title="Eliminar categoría">
+                                                <i class="fa fa-times"></i>
+                                            </a>
+                                        </td>
+                                    </tr>';
+                                }
+                            } else {
                                 echo '
                                 <tr>
-                                    <td>' . $categorias["id"] . '</td>
-                                    <td>' . $categorias["nombre"] . '</td>
-                                    <td>' . $categorias["usuario"] . '</td>
-                                    <td>' . $categorias["fecha"] . '</td>
-                                    <td>
-                                        <a href="' . site_url("categorias/editar") . '" class="btn btn-default btn-sm" title="Editar marca">
-                                            <i class="fa fa-pencil"></i>
-                                        </a>
-                                        <a href="' . site_url("categorias/eliminar") . '" class="btn btn-default btn-sm" title="Eliminar marca">
-                                            <i class="fa fa-times"></i>
-                                        </a>
+                                    <td colspan="5">
+                                        No se encontraron registros en tu catálogo de categorías de producto. 
+                                        Si deseas crear una ahora, haz click en el botón de abajo.<br><br>
+                                        <a href="' . site_url("categorias/crear") . '" class="btn btn-primary">Crear una categoría ahora</a>
                                     </td>
                                 </tr>';
                             }
@@ -60,3 +73,28 @@
         </div>
     </div>
 </div>
+
+<script>
+    function eliminarCategoria(url, nombreCategoria) {
+        bootbox.confirm({
+            title: 'Eliminar categoria: ' + nombreCategoria,
+            message: '¿Realmente deseas eliminar esta categoría?',
+            buttons: {
+                'cancel': {
+                    label: 'No realmente',
+                    className: 'btn-default'
+                },
+                'confirm': {
+                    label: 'Si, elimínala',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function(result) {
+                if (result) {
+                    window.location = url;
+                }
+            }
+        });
+
+    }
+</script>
