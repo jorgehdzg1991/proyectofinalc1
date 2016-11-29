b9879c76ab141bce0e673ef29bcb19c98e3cb6e5
Jorge Hernández García <jorgehdzg1991@gmail.com>
2016-11-28 21:11:54 -0600

    Se agregó consulta para obtener los movimientos en un almacén

--- application/models/Almacenes_model.php
+++ application/models/Almacenes_model.php
@@ -29,4 +29,43 @@ class Almacenes_model extends CI_Model
     {
         return $this->db->where("id", $id)->update("almacenes", ["estatus" => 0]) ? true : false;
     }
+
+    public function consultarExistenciasAlmacen($id) {
+        $sql = "
+        SELECT
+            alma.id AS idAlmacen,
+            alma.nombre AS nombreAlmacen,
+            timo.id AS idTipoMovimiento,
+            timo.nombre AS nombreTipoMovimiento,
+            prod.id AS idProducto,
+            prod.nombre AS nombreProducto,
+            prod.descripcion AS descripcionProducto,
+            movi.cantidad AS cantidadMovimiento,
+            movi.observaciones AS observacionesMovimiento,
+            movi.fecha AS fechaMovimiento,
+            cate.id AS idCategorias,
+            cate.nombre AS nombreCategoria,
+            marc.id AS idMarca,
+            marc.nombre AS nombreMarca,
+            unid.id AS idUnidad,
+            unid.nombre AS nombreUnidad,
+            unid.abreviatura AS abreviaturaUnidad,
+            usua.id AS idUsuarioMovimiento,
+            usua.nombre AS nombreUsuarioMovimiento
+        FROM
+            almacenes AS alma
+        INNER JOIN movimientos AS movi ON movi.almacenes_id = alma.id
+        INNER JOIN productos AS prod ON movi.productos_id = prod.id
+        INNER JOIN tiposmovimientos AS timo ON movi.tiposmovimientos_id = timo.id
+        INNER JOIN categorias AS cate ON prod.categorias_id = cate.id
+        INNER JOIN marcas AS marc ON prod.marcas_id = marc.id
+        INNER JOIN unidades AS unid ON prod.unidades_id = unid.id
+        INNER JOIN usuarios AS usua ON movi.usuarios_id = usua.id
+        WHERE
+            alma.id = $id";
+
+        $query = $this->db->query($sql);
+
+        return $query->result_array();
+    }
 }
