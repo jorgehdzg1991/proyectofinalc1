86488c374494648fdd067bf49e02695c85248e3b
Jorge Hernández García <jorgehdzg1991@gmail.com>
2016-12-03 12:11:10 -0600

    Se modifica la consulta de existencias en almacen

--- application/models/Almacenes_model.php
+++ application/models/Almacenes_model.php
@@ -30,39 +30,31 @@ class Almacenes_model extends CI_Model
         return $this->db->where("id", $id)->update("almacenes", ["estatus" => 0]) ? true : false;
     }
 
-    public function consultarExistenciasAlmacen($id) {
+    public function consultarExistenciasAlmacen($idAlmacen) {
         $sql = "
         SELECT
-            alma.id AS idAlmacen,
-            alma.nombre AS nombreAlmacen,
-            timo.id AS idTipoMovimiento,
-            timo.nombre AS nombreTipoMovimiento,
-            prod.id AS idProducto,
-            prod.nombre AS nombreProducto,
-            prod.descripcion AS descripcionProducto,
-            movi.cantidad AS cantidadMovimiento,
-            movi.observaciones AS observacionesMovimiento,
-            movi.fecha AS fechaMovimiento,
-            cate.id AS idCategorias,
-            cate.nombre AS nombreCategoria,
-            marc.id AS idMarca,
-            marc.nombre AS nombreMarca,
-            unid.id AS idUnidad,
-            unid.nombre AS nombreUnidad,
-            unid.abreviatura AS abreviaturaUnidad,
-            usua.id AS idUsuarioMovimiento,
-            usua.nombre AS nombreUsuarioMovimiento
+            prod.id AS id,
+            prod.nombre AS producto,
+            prod.descripcion AS descripcion,
+            cate.nombre AS categoria,
+            unid.nombre AS unidad,
+            marc.nombre AS marca,
+            (
+                SELECT
+                    SUM(movi.cantidad)
+                FROM
+                    movimientos AS movi
+                WHERE
+                    movi.productos_id = prod.id
+                AND almacenes_id = $idAlmacen
+            ) AS cantidad
         FROM
-            almacenes AS alma
-        INNER JOIN movimientos AS movi ON movi.almacenes_id = alma.id
-        INNER JOIN productos AS prod ON movi.productos_id = prod.id
-        INNER JOIN tiposmovimientos AS timo ON movi.tiposmovimientos_id = timo.id
+            productos AS prod
         INNER JOIN categorias AS cate ON prod.categorias_id = cate.id
-        INNER JOIN marcas AS marc ON prod.marcas_id = marc.id
         INNER JOIN unidades AS unid ON prod.unidades_id = unid.id
-        INNER JOIN usuarios AS usua ON movi.usuarios_id = usua.id
+        INNER JOIN marcas AS marc ON prod.marcas_id = marc.id
         WHERE
-            alma.id = $id";
+            prod.estatus = 1";
 
         $query = $this->db->query($sql);
 
